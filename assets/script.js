4a346a2f91cc84f0bd48d1cb1ce324eddb539973
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-03-29 14:49:12 -0600

    added API call for UVindex

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ var humidityEl = document.getElementById("humidity");
 var windSpeedEl = document.getElementById("windSpeed");
 var UVIndexEl = document.getElementById("UV-index");
 
-
 var APIKey = "d11b55d32ed7a61161ed887f7799ce29";
 
 // get current date
@@ -17,10 +16,12 @@ var yyyy = today.getFullYear();
 
 today = "(" + mm + "/" + dd + "/" + yyyy + ")";
 
+//function for current weather
 function getWeather(city) {
   var callURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
+    "&lat=&lon=" +
     "&units=imperial" /*<--convert units*/ +
     "&appid=" +
     APIKey;
@@ -43,13 +44,41 @@ function getWeather(city) {
       //parse results to page
 
       var currentCity = response.name + " " + today;
-      var currentHumidity = 'Humidity: ' + response.main.humidity + '%'; 
-      var currentTemp = 'Temperature: ' + response.main.temp + ' ℉';
-      var currentWind = 'Wind: ' + response.wind.speed + ' MPH';
+      var currentHumidity = "Humidity: " + response.main.humidity + "%";
+      var currentTemp = "Temperature: " + response.main.temp + " ℉";
+      var currentWind = "Wind: " + response.wind.speed + " MPH";
+      var UVCoordLat = response.coord.lat;
+      var UVCoordLon = response.coord.lon;
       currentWeatherEl.innerHTML = currentCity;
       tempEl.innerHTML = currentTemp;
       windSpeedEl.innerHTML = currentWind;
       humidityEl.innerHTML = currentHumidity;
+
+      var UVcall =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        UVCoordLat +
+        "&lon=" +
+        UVCoordLon +
+        "&units=imperial" + /*<--convert units*/
+        "&appid=" +
+        APIKey;
+
+        console.log(UVcall);
+
+        fetch(UVcall)
+        .then (function (UVresponse) {
+
+          return UVresponse.json();
+        })
+
+        .then (function (UVresponse) {
+          console.log(UVresponse);
+          currentUVI = 'UV Index: ' + UVresponse.current.uvi;
+          UVIndexEl.innerHTML = currentUVI;
+
+
+        })
+      
 
 
 
@@ -58,7 +87,37 @@ function getWeather(city) {
 
 searchEl.addEventListener("click", function () {
   const searchQ = cityInput.value;
-  getWeather(searchQ);
+  getWeather(searchQ); /*<--pass search query to call current weather */
+  getForecast(searchQ); /*<--pass search query to call forecast */
 
-  console.log(searchQ);
+  // console.log(searchQ);
 });
+
+//function for 5-day forcast
+
+function getForecast(city) {
+  var callURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial" /*<--convert units*/ +
+    "&appid=" +
+    APIKey;
+  // &units=imperial added to query URL for the US antiquated measurement system
+
+  console.log(callURL);
+
+  fetch(callURL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+
+    .then(function (response) {
+      console.log(response); //check results
+
+      //parse results to page
+    });
+}
